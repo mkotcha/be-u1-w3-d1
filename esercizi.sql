7df39440380564974dfcc5abf0c39684e06cb0f5
Matteo Falasca <matteo.falasca@emmek.org>
2023-10-16 14:45:30 +0200

    es4

--- esercizi.sql
+++ esercizi.sql
@@ -1,5 +1,4 @@
-CREATE TABLE public.clienti
-(
+CREATE TABLE public.clienti (
     numero_cliente integer NOT NULL,
     nome text NOT NULL,
     cognome text NOT NULL,
@@ -7,9 +6,7 @@ CREATE TABLE public.clienti
     regione_residenza text NOT NULL,
     PRIMARY KEY (numero_cliente)
 );
-
-CREATE TABLE public.prodotti
-(
+CREATE TABLE public.prodotti (
     id_prodotto integer NOT NULL,
     descrizione text NOT NULL,
     in_produzione boolean DEFAULT true,
@@ -18,9 +15,7 @@ CREATE TABLE public.prodotti
     data_disattivazione date,
     PRIMARY KEY (id_prodotto)
 );
-
-CREATE TABLE IF NOT EXISTS public.fatture
-(
+CREATE TABLE IF NOT EXISTS public.fatture (
     numero_fattura integer NOT NULL,
     tipologia text COLLATE pg_catalog."default" NOT NULL,
     importo double precision NOT NULL,
@@ -29,31 +24,30 @@ CREATE TABLE IF NOT EXISTS public.fatture
     data_fattura date NOT NULL,
     numero_fornitore integer NOT NULL,
     CONSTRAINT fatture_pkey PRIMARY KEY (numero_fattura),
-    CONSTRAINT clienti FOREIGN KEY (id_cliente)
-        REFERENCES public.clienti (numero_cliente) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT fornitori FOREIGN KEY (numero_fornitore)
-        REFERENCES public.fornitori (numero_fornitore) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
-
-CREATE TABLE public.fornitori
-(
+    CONSTRAINT clienti FOREIGN KEY (id_cliente) REFERENCES public.clienti (numero_cliente) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT fornitori FOREIGN KEY (numero_fornitore) REFERENCES public.fornitori (numero_fornitore) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION NOT VALID
+) CREATE TABLE public.fornitori (
     numero_fornitore integer NOT NULL,
     denominazione text NOT NULL,
     regione_residenza text NOT NULL,
     PRIMARY KEY (numero_fornitore)
 );
-
-
-SELECT nome,cognome FROM clienti WHERE data_nascita BETWEEN '1-1-1982' AND '31-12-1982'
-
-select numero_fattura from fatture where iva='22'
-
-
-SELECT COUNT(*) AS count_fatture, EXTRACT(YEAR FROM data_fattura) AS anno,  SUM(importo) AS totale_fatturato
+SELECT nome,
+    cognome
+FROM clienti
+WHERE data_nascita BETWEEN '1-1-1982' AND '31-12-1982'
+select numero_fattura
+from fatture
+where iva = '22'
+SELECT COUNT(*) AS count_fatture,
+    EXTRACT(
+        YEAR
+        FROM data_fattura
+    ) AS anno,
+    SUM(importo) AS totale_fatturato
 FROM fatture
 GROUP BY anno;
+SELECT *
+FROM prodotti
+WHERE in_produzione = 'true'
+    OR in_commercio = 'true'
